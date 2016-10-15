1761bec9343751ce400cf6ac21f027e411251e98
WEBER Antoine <ghostsmaker@hotmail.com>
2016-10-15 18:01:07 +0200

    ajout adminLTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import {CaissePage } from '../pages/caisse/caisse';
+import { CaissePage } from '../pages/caisse/caisse';
 import { TabsPage } from '../pages/tabs/tabs';
 
 var pages = [
   MyApp,
   AboutPage,
   ContactPage,
+  HomePage,
   CaissePage,
   TabsPage
 ]
